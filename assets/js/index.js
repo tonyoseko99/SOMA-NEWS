9325b110d63f6e29e00abf09a8e5ff4e150b98ed
Tonny Oseko <tonnyoseko0@gmail.com>
2022-06-21 14:59:36 +0300

    Add API key and respective urls to news categories

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,28 @@
-// API KEY
-const apiKey = '8d8d51e07d8d40078290e6f9a8c68ed4'
-
 // variables
 const general = document.getElementById('home')
 const sports = document.getElementById('sports')
 const technology = document.getElementById('technology')
 const business = document.getElementById('business')
+const health = document.getElementById('health')
+const science = document.getElementById('science')
 
 const searchNews = document.getElementById('search')
 const searchButton = document.getElementById('search-btn')
+
+const image = document.querySelector('image')
+const title = document.querySelector('#title')
+const description = document.querySelector('#description')
+const readMore = document.querySelector('#readMore')
+
+// API KEY
+const apiKey = '8d8d51e07d8d40078290e6f9a8c68ed4'
+
+const generalNews = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+const sportsNews = `https://newsapi.org/v2/top-headlines?category=sports&apiKey=${apiKey}`
+const technologyNews = `https://newsapi.org/v2/top-headlines?category=technology&apiKey=${apiKey}`
+const businessNews = `https://newsapi.org/v2/top-headlines?category=business&apiKey=${apiKey}`
+const healthNews = `https://newsapi.org/v2/top-headlines?category=health&apiKey=${apiKey}`
+const scienceNews = `https://newsapi.org/v2/top-headlines?category=science&apiKey=${apiKey}`
 
 // navbar items functionality
 general.addEventListener('click', function(){
@@ -24,6 +38,14 @@ technology.addEventListener('click', function(){
 });
 
 business.addEventListener('click', function(){
+
+});
+
+health.addEventListener('click', function(){
+
+});
+
+science.addEventListener('click', function(){
 
 });
 

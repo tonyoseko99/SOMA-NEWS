e6f077ced6bbbeb75403af5f84576e4a60fa5a7d
Tonny Oseko <tonnyoseko0@gmail.com>
2022-06-21 14:39:09 +0300

    Declare variables from the DOM in the JS file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,36 @@
-const apiKey = '8d8d51e07d8d40078290e6f9a8c68ed4'   // API Key
+// API KEY
+const apiKey = '8d8d51e07d8d40078290e6f9a8c68ed4'
+
+// variables
+const general = document.getElementById('home')
+const sports = document.getElementById('sports')
+const technology = document.getElementById('technology')
+const business = document.getElementById('business')
+
+const searchNews = document.getElementById('search')
+const searchButton = document.getElementById('search-btn')
+
+// navbar items functionality
+general.addEventListener('click', function(){
+
+});
+
+sports.addEventListener('click', function(){
+
+});
+
+technology.addEventListener('click', function(){
+
+});
+
+business.addEventListener('click', function(){
+
+});
+
+searchButton.addEventListener('click', function(){
+
+});
+
+searchNews.addEventListener('click', function(){
+
+});
